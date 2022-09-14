c3259aecdf1937c3b08787b9642c9de1441070f6
guswowh <guswowh88@gmail.com>
2022-09-15 02:06:07 +0900

    💄 design/해더 컴포넌트 레이아웃 구현/#25

--- src/components/layout/TheHeader/index.tsx
+++ src/components/layout/TheHeader/index.tsx
@@ -1,5 +1,16 @@
 import React from 'react'
+import * as S from './styles'
 
 export const TheHeader = () => {
-  return <div>헤더</div>
+  return (
+    <S.Wrapper>
+      <S.Inner>
+        <div>로고</div>
+        <div></div>
+        <div>
+          <S.SignUpBtn>회원가입</S.SignUpBtn>
+        </div>
+      </S.Inner>
+    </S.Wrapper>
+  )
 }
