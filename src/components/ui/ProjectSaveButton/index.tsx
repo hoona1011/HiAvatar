0be03276893a6a56c790b84cae9662be0f19c81c
hoona1011 <jasonpark90@naver.com>
2022-09-14 20:15:18 +0900

    💄 design: 프로젝트 저장 버튼 레이아웃 작업/#16

--- src/components/ui/ProjectSaveButton/index.tsx
+++ src/components/ui/ProjectSaveButton/index.tsx
@@ -1,5 +1,13 @@
 import React from 'react'
+import * as S from './style'
 
 export const ProjectSaveButton = () => {
-  return <div>ProjectSaveButton</div>
+  const onClickHandler = () => {
+    console.log('클릭')
+  }
+  return (
+    <S.Button onClick={onClickHandler}>
+      <span>프로젝트 저장</span>
+    </S.Button>
+  )
 }
---
+++ src/components/ui/ProjectSaveButton/style.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const Button = styled.button`
+  width: 13.5rem;
+  height: 4rem;
+  background-color: #666666;
+  border-radius: 1rem;
+  border: none;
+  cursor: pointer;
+
+  span {
+    color: #ffffff;
+    font-size: 1.6rem;
+    font-weight: 500;
+    line-height: 2rem;
+  }
+`
