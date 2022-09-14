544c089ef6356d67fe7a90dbd5e5ed9994f0bb9f
heeyoung-c <heeyoungchun.dev@gmail.com>
2022-09-14 15:41:43 +0900

    💄 design: 아바타 선택 컴포넌트 레이아웃 작업/#6

--- src/components/ui/AvatarSelector/index.tsx
+++ src/components/ui/AvatarSelector/index.tsx
@@ -1,5 +1,16 @@
 import React from 'react'
+import * as S from './style'
 
 export const AvatarSelector = () => {
-  return <div>AvatarSelector</div>
+  return (
+    <S.Container>
+      <S.Title>아바타를 선택해 주세요</S.Title>
+      <S.AvatarContainer>
+        <S.AvatarInner>
+          <S.Avatar></S.Avatar>
+          <S.AvatarName>아바타 1기</S.AvatarName>
+        </S.AvatarInner>
+      </S.AvatarContainer>
+    </S.Container>
+  )
 }
---
+++ src/components/ui/AvatarSelector/style.ts
@@ -0,0 +1,52 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 97.6rem;
+  height: 25.7rem;
+
+  padding: 3.2rem 0 0 3.2rem;
+  border-radius: 1rem;
+  background-color: #f5f5f5;
+
+  font-family: 'PretendardVariable';
+`
+
+export const Title = styled.div`
+  width: 19.2rem;
+  height: 2.5rem;
+
+  font-size: 2rem;
+`
+
+export const AvatarContainer = styled.div`
+  display: flex;
+
+  width: 91.2rem;
+  height: 16rem;
+  margin: 1.6rem 0 0;
+
+  overflow-x: scroll;
+  overflow-y: hidden;
+`
+
+export const AvatarInner = styled.div`
+  width: 21rem;
+  height: 16rem;
+  margin-right: 2.4rem;
+`
+
+export const Avatar = styled.div`
+  width: 21rem;
+  height: 13.3rem;
+
+  border-radius: 1rem;
+  background-color: #e7e7e7;
+`
+
+export const AvatarName = styled.div`
+  height: 1.9rem;
+  margin-top: 0.8rem;
+
+  font-size: 1.6rem;
+  text-align: center;
+`
