620c00ba136da5febac0d33b67fca767ec7b77f7
heeyoung-c <heeyoungchun.dev@gmail.com>
2022-09-14 16:36:06 +0900

    💄 design: 생성된 프로젝트 목록 컴포넌트 레이아웃 작업/#7

--- src/components/ui/ProjectList/index.tsx
+++ src/components/ui/ProjectList/index.tsx
@@ -1,5 +1,17 @@
 import React from 'react'
+import * as S from './style'
 
 export const ProjectList = () => {
-  return <div>ProjectList</div>
+  return (
+    <S.Container>
+      <S.Title>프로젝트를 생성하세요.</S.Title>
+      <S.ProjectContainer>
+        <S.ProjectInner>
+          <S.Project></S.Project>
+          <S.ProjectName>프로젝트 이름</S.ProjectName>
+          <S.ProjectDate>22.02.02</S.ProjectDate>
+        </S.ProjectInner>
+      </S.ProjectContainer>
+    </S.Container>
+  )
 }
---
+++ src/components/ui/ProjectList/style.ts
@@ -0,0 +1,53 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 120rem;
+  height: 33.6rem;
+  padding: 3.2rem 0 0 3.2rem;
+
+  border-radius: 2rem;
+  background-color: #f1f1f1;
+
+  font-family: 'PretendardVariable';
+`
+
+export const Title = styled.div`
+  width: max-content;
+
+  font-size: 2.1rem;
+`
+
+export const ProjectContainer = styled.div`
+  display: flex;
+
+  width: 113.6rem;
+  height: 23.9rem;
+  margin-top: 1.6rem;
+`
+
+export const ProjectInner = styled.div`
+  flex-shrink: 0;
+  width: 19.84rem;
+  margin-right: 3.6rem;
+`
+
+export const Project = styled.div`
+  height: 19rem;
+
+  background-color: #d9d9d9;
+  border-radius: 1rem;
+`
+
+export const ProjectName = styled.div`
+  margin-top: 0.9rem;
+
+  font-size: 1.6rem;
+  text-align: center;
+  line-height: 2rem;
+`
+
+export const ProjectDate = styled.div`
+  font-size: 1.2rem;
+  text-align: center;
+  line-height: 1.6rem;
+`
