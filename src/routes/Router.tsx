9b3500d17d88e6a045ba80d1e07731a4250a3a43
hoona1011 <jasonpark90@naver.com>
2022-09-14 22:14:08 +0900

    ✨ feat: 페이지 라우트 경로 설정/#20

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -15,7 +15,12 @@ const Router = () => {
   return (
     <>
       <Routes>
-        <Route path='/' element={<SignIn />} />
+        <Route path='/' element={<LandingPage />} />
+        <Route path='my-page' element={<MyPage />} />
+        <Route path='project-avatar' element={<ProjectAvatar />} />
+        <Route path='project-history' element={<ProjectHistory />} />
+        <Route path='project-text-edit' element={<ProjectTextEdit />} />
+        <Route path='project-text-enter' element={<ProjectTextEnter />} />
         <Route path='sign-in' element={<SignIn />} />
         <Route path='sign-up' element={<SignUp />} />
       </Routes>
