9b8d2d47687b94693abb46c2d3dacd71396cf9d9
hoona1011 <jasonpark90@naver.com>
2022-09-15 21:51:05 +0900

    ✨ feat: 음성 업로드 버튼 테스트/#24

--- src/components/ui/VoiceUploadButton/index.tsx
+++ src/components/ui/VoiceUploadButton/index.tsx
@@ -1,5 +1,24 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import * as S from './style'
 
 export const VoiceUploadButton = () => {
-  return <div>VoiceUploadButton</div>
+  const inputRef = useRef<HTMLInputElement | null>(null)
+
+  const onClickHandler = () => {
+    inputRef.current!.click()
+  }
+  const onUploadHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.files![0])
+  }
+  return (
+    <S.Button onClick={onClickHandler}>
+      <span>+ 음성 업로드</span>
+      <input
+        type='file'
+        accept='audio/*'
+        ref={inputRef}
+        onChange={onUploadHandler}
+      />
+    </S.Button>
+  )
 }
