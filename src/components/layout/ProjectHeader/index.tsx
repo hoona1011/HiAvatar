bd55b49bb41af9237dd55cfc5bcfbb9d431b11da
guswowh <guswowh88@gmail.com>
2022-09-15 17:52:14 +0900

    💄 design: 프로덕트 해더 컴포넌트 레이아웃 구현/#26

--- src/components/layout/ProjectHeader/index.tsx
+++ src/components/layout/ProjectHeader/index.tsx
@@ -1,5 +1,21 @@
 import React from 'react'
+import * as S from './style'
+import { ProjectSaveButton } from '../../ui/ProjectSaveButton'
+import { EditIcon } from '../../Icons'
 
 export const ProjectHeader = () => {
-  return <div>ProjectHeader</div>
+  return (
+    <S.Wrapper>
+      <div>로고</div>
+      <div>
+        <span>프로젝트명</span>
+        <S.EditBtn>
+          <EditIcon width='20' height='19' />
+        </S.EditBtn>
+      </div>
+      <div>
+        <ProjectSaveButton />
+      </div>
+    </S.Wrapper>
+  )
 }
