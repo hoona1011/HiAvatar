f9a61dbe0e512a6f1b80a10575bcfc7d6010349b
guswowh <guswowh88@gmail.com>
2022-09-15 15:56:46 +0900

    🐛 fix: 폰트 적용 문제 해결/#25

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -1,19 +1,37 @@
 import { createGlobalStyle } from 'styled-components'
+import reset from 'styled-reset'
 
 export default createGlobalStyle`
+${reset}
 
- * {
-    box-sizing: border-box;
-    font-family: 'Pretendard Variable';
-    
-  }
-
-  html {
-    font-size: 62.5%;
-    /* rem 값의 기준을 잡기 위함 16px -> 10px */
-  }
-
-  body {
-    margin: 0;
-  }
+* {
+  box-sizing: border-box;
+  font-family: 'Pretendard Variable' !important;
+}
+html {
+  font-size: 62.5%;
+  /* rem 값의 기준을 잡기 위함 16px -> 10px */
+}
+body {
+  margin: 0;
+}
+input, textarea { 
+  -moz-user-select: auto;
+  -webkit-user-select: auto;
+  -ms-user-select: auto;
+  user-select: auto;
+}
+input:focus {
+  outline: none;
+}
+button {
+  border: none;
+  background: none;
+  padding: 0;
+  cursor: pointer;
+}
+a{
+  text-decoration: none;
+  color: inherit;
+}
 `
