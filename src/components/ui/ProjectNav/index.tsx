1ff0dce79074aabc1c7b0507104cff066367e65c
boksooni <8545652@naver.com>
2022-09-16 14:21:09 +0900

    🎨 format: 코드 수정

--- src/components/ui/ProjectNav/index.tsx
+++ src/components/ui/ProjectNav/index.tsx
@@ -1,34 +1,34 @@
 import React from 'react'
 import * as S from './style'
-import { NavIon01, NavIon02, NavIon03, NavIon04 } from '../../Icons'
+import { NavIcon01, NavIcon02, NavIcon03, NavIcon04 } from '../../Icons'
 
 export const ProjectNav = () => {
   return (
     <S.ProjectNavContainer>
       <S.ProjectNavStepContainer>
         <S.ProjectNavStepIcon>
-          <NavIon01 width='31' height='30' />
+          <NavIcon01 width='31' height='30' />
         </S.ProjectNavStepIcon>
         <S.ProjectNavText>01.텍스트 입력</S.ProjectNavText>
       </S.ProjectNavStepContainer>
       <S.ProjectNavHr />
       <S.ProjectNavStepContainer>
         <S.ProjectNavStepIcon>
-          <NavIon02 width='31' height='30' />
+          <NavIcon02 width='31' height='30' />
         </S.ProjectNavStepIcon>
         <S.ProjectNavText>02.텍스트 수정</S.ProjectNavText>
       </S.ProjectNavStepContainer>
       <S.ProjectNavHr />
       <S.ProjectNavStepContainer>
         <S.ProjectNavStepIcon>
-          <NavIon03 width='31' height='30' />
+          <NavIcon03 width='31' height='30' />
         </S.ProjectNavStepIcon>
         <S.ProjectNavText>03.아바타 선택</S.ProjectNavText>
       </S.ProjectNavStepContainer>
       <S.ProjectNavHr />
       <S.ProjectNavStepContainer>
         <S.ProjectNavStepIcon>
-          <NavIon04 width='31' height='30' />
+          <NavIcon04 width='31' height='30' />
         </S.ProjectNavStepIcon>
         <S.ProjectNavText>04.제작 완료</S.ProjectNavText>
       </S.ProjectNavStepContainer>
