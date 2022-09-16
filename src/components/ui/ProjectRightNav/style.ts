0d0d28b7123a27b37ee4f90c4a8ada842addc0e0
heeyoung-c <heeyoungchun.dev@gmail.com>
2022-09-16 21:45:33 +0900

    💄 design: 공통 프로젝트 오른쪽 Nav 레이아웃 작업/#31

---
+++ src/components/ui/ProjectRightNav/style.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 38.4rem;
+  height: 71.2rem;
+  padding: 2.4rem;
+  background-color: #f3f3f3;
+`
+
+export const Inner = styled.div`
+  width: 33.6rem;
+  height: 59.2rem;
+  margin-bottom: 2.4rem;
+`
+
+export const BtnContainer = styled.div`
+  border: 0.1rem solid;
+
+  width: 33.6rem;
+  height: 4.8rem;
+`
