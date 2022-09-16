7a10649c3b6477cd9488ba8b4edb8cf878883420
boksooni <8545652@naver.com>
2022-09-16 13:44:50 +0900

    💄 design: ProjectNav/#8

---
+++ src/components/ui/ProjectNav/style.ts
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+
+export const ProjectNavContainer = styled.div`
+  background-color: #fafafa;
+  border-radius: 1rem;
+  padding: 1rem;
+  display: flex;
+  flex-direction: row;
+  width: 78rem;
+`
+export const ProjectNavStepContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+export const ProjectNavStepIcon = styled.div`
+  margin: auto;
+`
+export const ProjectNavText = styled.div`
+  margin-top: 0.3rem;
+`
+export const ProjectNavHr = styled.hr`
+  background-color: #9d9d9d;
+  width: 11rem;
+  height: 0.2rem;
+  border: none;
+  margin: auto;
+`
