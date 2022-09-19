9b8b11580c5bc0353da681237f3a4737b8b8994e
hoona1011 <jasonpark90@naver.com>
2022-09-19 13:58:18 +0900

    🔧 conf: vite 절대경로 설정/#52
    Co-authored-by: guswowh <guswowh@users.noreply.github.com>
    Co-authored-by: Heeyoung Chun <heeyoungchun.dev@gmail.com>
    Co-authored-by: boksooni <boksooni@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import * as path from 'path'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   // resolve: {
+//   //   alias: [{ find: '~', replacement: `${__dirname}/src` }]
+//   // }
+
+//   resolve: {
+//     alias: {
+//       '~': path.resolve(__dirname, 'src')
+//     }
+//   }
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react(), tsconfigPaths()]
 })
+
+// export default defineConfig({
+//   plugins: [tsconfigPaths()],
+// })
