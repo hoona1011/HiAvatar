3ee6dbb821c28f6e047fdedd2c63010c5a261b6e
hoona1011 <jasonpark90@naver.com>
2022-09-19 13:58:08 +0900

    ♻️ refactor: 타입 설정 파일 생성/#52
    Co-authored-by: guswowh <guswowh@users.noreply.github.com>
    Co-authored-by: Heeyoung Chun <heeyoungchun.dev@gmail.com>
    Co-authored-by: boksooni <boksooni@users.noreply.github.com>

---
+++ src/type.ts
@@ -0,0 +1,20 @@
+//
+
+// 지훈
+export interface RadioButtonProps {
+  buttonType: string
+  value: string
+  content: string
+}
+
+// const abc = 123
+// export default abc
+
+// 희영
+export interface ProjectRightNavProps {
+  renderType: string
+}
+
+//
+
+// 수연
