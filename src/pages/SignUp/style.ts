15b5e2bd2360a7305550d3b0d38f32bc72113b38
boksooni <8545652@naver.com>
2022-09-20 22:13:41 +0900

    💄 design: SignUp/#54

---
+++ src/pages/SignUp/style.ts
@@ -0,0 +1,78 @@
+import styled from 'styled-components'
+
+export const SignUpContainer = styled.div`
+  display: flex;
+  /* text-align: center; */
+`
+export const SignUpWrapper = styled.div`
+  margin: 0 auto;
+  /* text-align: center; */
+`
+export const SignUpText = styled.div`
+  font-size: 4.8rem;
+  font-weight: 700;
+  margin-bottom: 3rem;
+  text-align: center;
+`
+export const SignUpEmailWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+`
+export const SignUpText2 = styled.div`
+  font-size: 1.5rem;
+  font-weight: 600;
+  margin-bottom: 0.5rem;
+`
+export const PositionWrap = styled.div`
+  position: relative;
+`
+export const SignUpInput = styled.input`
+  width: 38.4rem;
+  height: 5.6rem;
+  border-radius: 0.8rem;
+  border: 0.1rem solid #000;
+  font-size: 2rem;
+  margin-bottom: 0.5rem;
+`
+export const IdCheckBtn = styled.button`
+  background-color: #757575;
+  color: #fff;
+  width: 6.5rem;
+  height: 3rem;
+  border-radius: 0.5rem;
+
+  position: absolute;
+  left: 30.3rem;
+  top: 1.3rem;
+`
+export const PasswordText = styled.div`
+  font-size: 1.5rem;
+`
+export const SignUpPwInput = styled.input`
+  width: 38.4rem;
+  height: 5.6rem;
+  border-radius: 0.8rem;
+  border: 0.1rem solid #000;
+  font-size: 2rem;
+  margin-bottom: 0.5rem;
+`
+export const PasswordCheckBtn = styled.button`
+  position: absolute;
+  top: 1.8rem;
+  left: 34rem;
+  cursor: default;
+`
+export const SignUpBtn = styled.button`
+  width: 38.4rem;
+  height: 6rem;
+  border-radius: 1rem;
+  background-color: #757575;
+  color: #fff;
+  font-size: 2rem;
+  margin-top: 1rem;
+`
+export const TempSpan = styled.span`
+  font-weight: 600;
+  margin-bottom: 2.5rem;
+`
