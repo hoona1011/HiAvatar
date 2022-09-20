4744b598f75146457b2e72ffe5c3caa8d48abf2a
heeyoung-c <heeyoungchun.dev@gmail.com>
2022-09-20 15:49:36 +0900

    ✨ feat: 아바타 선택 슬라이드 처리/#48

--- src/components/ui/AvatarSelector/index.tsx
+++ src/components/ui/AvatarSelector/index.tsx
@@ -1,40 +1,37 @@
 import React from 'react'
 import * as S from './style'
 
+import { MainSlider } from '../../../components'
+
 export const AvatarSelector = () => {
+  const data = [
+    {
+      name: '아바타 1'
+    },
+    {
+      name: '아바타 2'
+    },
+    {
+      name: '아바타 3'
+    },
+    {
+      name: '아바타 4'
+    },
+    {
+      name: '아바타 5'
+    },
+    {
+      name: '아바타 6'
+    },
+    {
+      name: '아바타 7'
+    }
+  ]
+
   return (
     <S.Container>
       <S.Title>아바타를 선택해 주세요</S.Title>
-      <S.AvatarContainer>
-        <S.AvatarInner>
-          <S.Avatar />
-          <S.AvatarName>아바타 1기</S.AvatarName>
-        </S.AvatarInner>
-        <S.AvatarInner>
-          <S.Avatar />
-          <S.AvatarName>아바타 2기</S.AvatarName>
-        </S.AvatarInner>
-        <S.AvatarInner>
-          <S.Avatar />
-          <S.AvatarName>아바타 3기</S.AvatarName>
-        </S.AvatarInner>
-        <S.AvatarInner>
-          <S.Avatar />
-          <S.AvatarName>아바타 4기</S.AvatarName>
-        </S.AvatarInner>
-        <S.AvatarInner>
-          <S.Avatar />
-          <S.AvatarName>아바타 5기</S.AvatarName>
-        </S.AvatarInner>
-        <S.AvatarInner>
-          <S.Avatar />
-          <S.AvatarName>아바타 6기</S.AvatarName>
-        </S.AvatarInner>
-        <S.AvatarInner>
-          <S.Avatar />
-          <S.AvatarName>아바타 7기</S.AvatarName>
-        </S.AvatarInner>
-      </S.AvatarContainer>
+      <MainSlider data={data} />
     </S.Container>
   )
 }
--- src/components/ui/AvatarSelector/style.ts
+++ src/components/ui/AvatarSelector/style.ts
@@ -2,48 +2,16 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   height: 18rem;
+  padding: 1.6rem 2.4rem 0;
 
-  padding: 1.6rem 0 0 2.4rem;
   border-radius: 1rem;
   background-color: #f5f5f5;
   margin-bottom: 2.4rem;
 `
 
 export const Title = styled.div`
-  width: 13.7rem;
   height: 1.8rem;
 
   font-size: 1.5rem;
   font-weight: 600;
-`
-
-export const AvatarContainer = styled.div`
-  display: flex;
-
-  width: 74.4rem;
-  margin-top: 0.8rem;
-
-  overflow-y: hidden;
-`
-
-export const AvatarInner = styled.div`
-  height: 12.2rem;
-  margin-right: 0.8rem;
-`
-
-export const Avatar = styled.div`
-  width: 18rem;
-  height: 10rem;
-
-  border-radius: 1rem;
-  background-color: #e7e7e7;
-`
-
-export const AvatarName = styled.div`
-  height: 1.4rem;
-  margin-top: 0.8rem;
-
-  font-size: 1.2rem;
-  font-weight: 600;
-  text-align: center;
 `
