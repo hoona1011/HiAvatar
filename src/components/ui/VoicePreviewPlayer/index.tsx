e1a602ed0ff9b27de38f839f4e3ec0d55bd169d4
guswowh <guswowh88@gmail.com>
2022-09-21 14:03:49 +0900

    ✨ feat: 오디오 플레이어 기능 변경/#60

--- src/components/ui/VoicePreviewPlayer/index.tsx
+++ src/components/ui/VoicePreviewPlayer/index.tsx
@@ -3,12 +3,23 @@ import {
   VoicePrePlayIcon,
   VoicePreRewindIcon
 } from 'components/Icons'
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import AudioPlayer from 'react-h5-audio-player'
 import 'react-h5-audio-player/lib/styles.css'
 import * as S from './style'
+
+// 타입스크립트 추가 예정
+
 export const VoicePreviewPlayer = () => {
   const VoiceData = '/src/assets/test.mp3'
+  const player = useRef()
+
+  const stop = () => {
+    const audio = player.current.audio.current
+    audio.pause()
+    audio.currentTime = 0
+  }
+
   return (
     <>
       <S.Title>합친 음성을 미리 들을 수 있어요</S.Title>
@@ -16,14 +27,19 @@ export const VoicePreviewPlayer = () => {
         <AudioPlayer
           customIcons={{
             play: <VoicePrePlayIcon width='25' height='24' />,
-            rewind: <VoicePreRewindIcon width='25' height='24' />,
-            forward: <VoicePreForwardIcon width='25' height='24' />
+            previous: <VoicePreRewindIcon width='25' height='24' />,
+            next: <VoicePreForwardIcon width='25' height='24' />
           }}
+          customAdditionalControls={[<button onClick={stop}>stop</button>]}
           customVolumeControls={[]}
-          customAdditionalControls={[]}
+          showJumpControls={false}
+          showSkipControls={false}
           layout='horizontal-reverse'
           src={VoiceData}
-          onPlay={(e) => console.log('onPlay')}
+          ref={player}
+          onPlay={(e) => {
+            console.log(e)
+          }}
           // other props here
         />
       </S.CustomStyle>
