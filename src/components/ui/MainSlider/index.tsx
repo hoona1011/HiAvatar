1c605afa9b23a884810068304c53e51dcab5eb14
heeyoung-c <heeyoungchun.dev@gmail.com>
2022-09-22 22:50:18 +0900

    ✨ feat: AvatarSelector 데이터 연결/#71

--- src/components/ui/MainSlider/index.tsx
+++ src/components/ui/MainSlider/index.tsx
@@ -1,23 +1,47 @@
 import React, { useState, useEffect, useRef } from 'react'
 import * as S from './style'
+import { MainSliderProps } from 'index'
 import SwiperCore, { Navigation } from 'swiper'
 import { Swiper, SwiperSlide } from 'swiper/react'
-import 'swiper/components/navigation/navigation.min.css'
 import { SwiperNextIcon, SwiperPrevIcon } from 'components/Icons'
-import { MainSliderProps } from 'index'
-import avatarData from 'avatarDb'
+import 'swiper/components/navigation/navigation.min.css'
 import {
-  changeAvatarDetailList,
+  changeTotalAvatarData,
   changeAvatarList,
+  changeAvatarDetailList,
+  changeBackgroundList,
   useAvatar
 } from 'store/slices/avatarSlice'
+import { useGetAvatarQuery } from 'api/avatarApi'
 
 export const MainSlider = ({ renderType }: MainSliderProps) => {
-  // swiper 관련
-  SwiperCore.use([Navigation])
+  const [swiperSetting, setSwiperSetting] = useState<Swiper | null>(null)
   const prevRef = useRef<HTMLButtonElement>(null)
   const nextRef = useRef<HTMLButtonElement>(null)
-  const [swiperSetting, setSwiperSetting] = useState<Swiper | null>(null)
+  const { data: avatarData, isLoading, isError } = useGetAvatarQuery()
+  const { avatarList, avatarDetailList, dispatch } = useAvatar()
+  SwiperCore.use([Navigation])
+
+  const avatarListHandler = () => {
+    if (avatarData) {
+      const data = Object.entries(avatarData.dummyData) // 2차원 배열로 만들어주기
+      const backgroundList = data.pop() // 마지막 요소는 `backgroundList` 이므로 따로 보관
+      dispatch(changeTotalAvatarData({ diff: avatarData }))
+      dispatch(changeAvatarList({ diff: data }))
+      dispatch(changeBackgroundList({ diff: backgroundList[1] }))
+    }
+  }
+
+  const avatarDetailListHandler = (selectedAvatar: string) => {
+    const data = avatarList.filter((avatar) => avatar[0] === selectedAvatar)
+    dispatch(changeAvatarDetailList({ diff: data[0][1] }))
+  }
+
+  useEffect(() => {
+    avatarData && avatarListHandler()
+  }, [avatarData])
+
+  // swiper 관련
   useEffect(() => {
     if (!swiperSetting) {
       setSwiperSetting({
@@ -40,42 +64,32 @@ export const MainSlider = ({ renderType }: MainSliderProps) => {
     }
   }, [swiperSetting])
 
-  const [dummyData, setDummyData] = useState(avatarData[0].data.dummyData)
-  const { avatarList, avatarDetailList, dispatch } = useAvatar()
-
-  const avatarListHandler = () => {
-    const data = Object.entries(dummyData) // 2차원 배열로 만들어주기
-    data.pop() // 마지막 요소는 `backgroundList` 이므로 제거
-    dispatch(changeAvatarList({ diff: data }))
+  if (isLoading || !avatarData) {
+    return <div>로딩중</div>
   }
 
-  const avatarDetailListHandler = (selectedAvatar: string) => {
-    const data = avatarList.filter((avatar) => avatar[0] === selectedAvatar)
-    dispatch(changeAvatarDetailList({ diff: data[0][1] }))
+  if (isError) {
+    return <div>에러</div>
   }
-
-  useEffect(() => {
-    avatarListHandler()
-  }, [])
-
   const RenderValue = () => {
     switch (renderType) {
       case 'AvatarSelector':
         return (
           <Swiper {...swiperSetting}>
-            {avatarList.map((avatar, index: string) => {
-              return (
-                <SwiperSlide
-                  key={avatar[0]}
-                  onClick={() => {
-                    avatarDetailListHandler(avatar[0])
-                  }}
-                >
-                  <S.Content />
-                  <S.Name>아바타 {index + 1}</S.Name>
-                </SwiperSlide>
-              )
-            })}
+            {avatarList &&
+              avatarList.map((avatar, index: string) => {
+                return (
+                  <SwiperSlide
+                    key={avatar[0]}
+                    onClick={() => {
+                      avatarDetailListHandler(avatar[0])
+                    }}
+                  >
+                    <S.Content />
+                    <S.Name>아바타 {index + 1}</S.Name>
+                  </SwiperSlide>
+                )
+              })}
           </Swiper>
         )
       case 'AvatarSort-1':
@@ -128,10 +142,10 @@ export const MainSlider = ({ renderType }: MainSliderProps) => {
       <S.PrevBtnContainer ref={prevRef}>
         <SwiperPrevIcon width='32' height='32' />
       </S.PrevBtnContainer>
-      {swiperSetting && RenderValue()}
       <S.NextBtnContainer ref={nextRef}>
         <SwiperNextIcon width='32' height='32' />
       </S.NextBtnContainer>
+      {swiperSetting && RenderValue()}
     </S.Container>
   )
 }
