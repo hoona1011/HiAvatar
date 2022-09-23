267c1bfe8c12a357023140035e166dcf12705d1b
boksooni <8545652@naver.com>
2022-09-23 14:16:58 +0900

    💄 design: MyPage/#78

--- src/pages/Mypage/index.tsx
+++ src/pages/Mypage/index.tsx
@@ -1,5 +1,40 @@
 import React from 'react'
+import * as S from './style'
+import { SignUpIcon } from 'components/Icons'
 
 export const MyPage = () => {
-  return <div>MyPage</div>
+  return (
+    <S.Container>
+      <S.Wrapper>
+        <S.TopText>계정관리</S.TopText>
+        <S.MyPwWrapper>
+          <S.MidText>기존 비밀번호</S.MidText>
+          <S.PwInput type='text' />
+        </S.MyPwWrapper>
+        <S.ChangePwWrapper>
+          <S.MidText>변경할 비밀번호</S.MidText>
+          <S.PositionWrap>
+            <S.PwInput type='paswword' />
+            <S.PwCheckBtn>
+              <SignUpIcon width='18' height='18' />
+            </S.PwCheckBtn>
+          </S.PositionWrap>
+          <S.TempSpan>
+            * 영문 대소문자, 숫자, 특수문자 중 2가지 이상 조합(8~50자)
+          </S.TempSpan>
+        </S.ChangePwWrapper>
+        <S.ChangePwWrapper>
+          <S.MidText>새로운 비밀번호 확인</S.MidText>
+          <S.PositionWrap>
+            <S.PwInput type='password' />
+            <S.PwCheckBtn>
+              <SignUpIcon width='18' height='18' />
+            </S.PwCheckBtn>
+          </S.PositionWrap>
+          <S.NewPwSpan>(비밀번호 중복 확인 멘트)</S.NewPwSpan>
+        </S.ChangePwWrapper>
+        <S.SubmitBtn>비밀번호 변경하기</S.SubmitBtn>
+      </S.Wrapper>
+    </S.Container>
+  )
 }
