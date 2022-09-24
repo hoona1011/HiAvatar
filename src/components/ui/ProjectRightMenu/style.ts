bf28ffd1136846b917ae5fa012438c29df72d18f
hoona1011 <jasonpark90@naver.com>
2022-09-24 15:52:55 +0900

    💄 design: ProjectRightMenu 스타일링 작업/#84

--- src/components/ui/ProjectRightMenu/style.ts
+++ src/components/ui/ProjectRightMenu/style.ts
@@ -1,7 +1,6 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  border: 1px solid red;
   display: flex;
   justify-content: space-evenly;
   margin-bottom: 3.4rem;
