b2f19355125f8a6d037789f90331ecb9419304f6
hoona1011 <jasonpark90@naver.com>
2022-09-26 15:44:59 +0900

    feat: 음성 업로드 버튼 기능 구현/#95

--- src/components/ui/VoiceUploadButton/index.tsx
+++ src/components/ui/VoiceUploadButton/index.tsx
@@ -1,16 +1,36 @@
-import { VoiceIcon } from 'components/Icons'
 import React, { useRef } from 'react'
+import { VoiceIcon } from 'components/Icons'
+import { useNavigate } from 'react-router-dom'
 import * as S from './style'
 
 export const VoiceUploadButton = () => {
   const inputRef = useRef<HTMLInputElement | null>(null)
+  const navigate = useNavigate()
 
   const onClickHandler = () => {
-    inputRef.current!.click()
+    confirm(
+      '음성 업로드 기능은 상세 설정을 할 수 없습니다. 계속하시겠습니까?'
+    ) && inputRef.current!.click()
   }
   const onUploadHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(e.target.files![0])
+    const audioFile = e.target.files![0]
+    console.log('업로드된 파일 정보: ', audioFile)
+    const fileReader = new FileReader()
+    //Temporary code to be modified later
+
+    fileReader.onload = (e) => {
+      console.log('바이너리', e.target.result)
+      const confirmMessage = `파일명: ${audioFile.name}
+      아바타 선택 페이지로 넘어가시겠습니까?
+    `
+      if (confirm(confirmMessage)) {
+        navigate('/project-avatar')
+      }
+    }
+
+    fileReader.readAsBinaryString(audioFile)
   }
+
   return (
     <S.Button onClick={onClickHandler}>
       <VoiceIcon width='2.4rem' height='2.4rem' />
