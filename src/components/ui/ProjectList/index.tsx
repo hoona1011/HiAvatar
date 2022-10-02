9192840544a8b325f41eba3b1dc5ee8f15a21641
heeyoung-c <heeyoungchun.dev@gmail.com>
2022-10-02 13:41:38 +0900

    ✨ feat: ProjectList 렌더링 데이터 수정/#112

--- src/components/ui/ProjectList/index.tsx
+++ src/components/ui/ProjectList/index.tsx
@@ -1,24 +1,40 @@
 import React from 'react'
 import * as S from './style'
 import { TooltipIcon } from 'components/Icons'
+import { useHistory } from 'store/slices/historySlice'
 
 export const ProjectList = () => {
+  const { projects } = useHistory()
+
+  const renderValue = () => {
+    if (projects.length) {
+      return projects.map((project) => {
+        const { projectId, projectName, lastModifiedAt } = project
+
+        return (
+          <S.Inner key={projectId}>
+            <S.Project></S.Project>
+            <S.Name>{projectName}</S.Name>
+            <S.Date>{lastModifiedAt}</S.Date>
+          </S.Inner>
+        )
+      })
+    } else {
+      return (
+        <S.NoProject>
+          프로젝트가 없습니다. 새 프로젝트를 생성하세요.
+        </S.NoProject>
+      )
+    }
+  }
+
   return (
     <S.Container>
       <S.Top>
         <TooltipIcon width='1.6rem' height='1.6rem' />
         <S.Title>프로젝트 히스토리를 확인하세요.</S.Title>
       </S.Top>
-      <S.ProjectContainer>
-        {/* <S.NoProject>
-          프로젝트가 없습니다. 새 프로젝트를 생성하세요.
-        </S.NoProject> */}
-        <S.Inner>
-          <S.Project></S.Project>
-          <S.Name>프로젝트 이름</S.Name>
-          <S.Date>22.02.02</S.Date>
-        </S.Inner>
-      </S.ProjectContainer>
+      <S.ProjectContainer>{renderValue()}</S.ProjectContainer>
     </S.Container>
   )
 }
