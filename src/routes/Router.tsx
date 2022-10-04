5b68b4a6862be56e2e0c7477e3b7bbccb09a4ebf
heeyoung-c <heeyoungchun.dev@gmail.com>
2022-10-04 19:28:11 +0900

    ✨ feat: `Route path`에 `:projectId`추가/#124

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -15,9 +15,12 @@ const Router = () => {
       <Routes>
         <Route path='/' element={<LandingPage />} />
         <Route path='my-page' element={<MyPage />} />
-        <Route path='project-avatar' element={<ProjectAvatar />} />
         <Route path='project-history' element={<ProjectHistory />} />
-        <Route path='project-text-edit' element={<ProjectTextEdit />} />
+        <Route
+          path='project-text-edit/:projectId'
+          element={<ProjectTextEdit />}
+        />
+        <Route path='project-avatar/:projectId' element={<ProjectAvatar />} />
         <Route path='sign-in' element={<SignIn />} />
         <Route path='sign-up' element={<SignUp />} />
       </Routes>
