66367f6577b62084e3b22b809173c05ca2971a45
heeyoung-c <heeyoungchun.dev@gmail.com>
2022-10-04 19:28:37 +0900

    👽️ api: projectList 데이터 가져오기

--- src/api/historyApi.ts
+++ src/api/historyApi.ts
@@ -2,11 +2,20 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import type { Projects } from 'index'
 
 const url = import.meta.env.VITE_SERVICE_URL
+const accessToken = import.meta.env.VITE_TOKEN
 
 export const historyApi = createApi({
   reducerPath: 'historyApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: url
+    baseUrl: url,
+    credentials: 'omit',
+    prepareHeaders: (headers, { getState }) => {
+      const accessTokenCookies = accessToken
+      if (accessTokenCookies) {
+        headers.set('Authorization', `Bearer ${accessTokenCookies}`)
+      }
+      return headers
+    }
   }),
   endpoints: (builder) => ({
     getHistory: builder.query({
