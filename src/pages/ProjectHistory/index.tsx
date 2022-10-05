d7fe99043030192321e25895c52f9a01a28593ba
heeyoung-c <heeyoungchun.dev@gmail.com>
2022-10-05 23:39:29 +0900

    ♻️ refactor: history 관련 데이터만 가져오던 컴포넌트 내용 이동/#128

--- src/pages/ProjectHistory/index.tsx
+++ src/pages/ProjectHistory/index.tsx
@@ -1,17 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import * as S from './style'
 import {
   TheHeader,
   ProjectList,
   ProjectVideoList,
-  ProjectHistoryData
+  CreateProjectButton
 } from 'components'
-import { CreateProjectButton } from 'components/ui/CreateProjectButton'
+import { useGetHistoryQuery } from 'api/historyApi'
+import { changeHistory, useHistory } from 'store/slices/historySlice'
 
 export const ProjectHistory = () => {
+  const { data, isLoading, isError } = useGetHistoryQuery()
+  const { dispatch } = useHistory()
+
+  const changeHistoryHandler = () => {
+    data && dispatch(changeHistory({ diff: data }))
+  }
+
+  useEffect(() => {
+    data && changeHistoryHandler()
+  }, [data])
+
+  if (isLoading || !data) {
+    return <>{/* 로딩 관련 추가 예정 */}</>
+  }
+
+  if (isError) {
+    return <></>
+  }
   return (
     <>
-      <ProjectHistoryData />
       <TheHeader />
       <S.Container>
         <CreateProjectButton />
