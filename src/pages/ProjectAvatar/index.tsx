1d7913abc3f4158ea8c288f7af017982ef6c1d46
heeyoung-c <heeyoungchun.dev@gmail.com>
2022-10-05 23:39:17 +0900

    ♻️ refactor: avatar 관련 데이터만 가져오던 컴포넌트 내용 이동/#128

--- src/pages/ProjectAvatar/index.tsx
+++ src/pages/ProjectAvatar/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {
   TheHeader,
   AvatarSelector,
@@ -6,15 +6,52 @@ import {
   ProjectRightNav,
   AvatarBg,
   ProjectNav,
-  ProjectAvatarModal,
-  ProjectAvatarData
+  ProjectAvatarModal
 } from 'components'
+import { useGetAvatarQuery } from 'api/avatarApi'
+import { useLocation } from 'react-router-dom'
+import {
+  changeAvatarDetailList,
+  changeTotalAvatarData,
+  useAvatar
+} from 'store/slices/avatarSlice'
 import * as S from './style'
 
 export const ProjectAvatar = () => {
+  const location = useLocation()
+  const projectId = location.pathname.replace(/[^0-9]/g, '')
+  const { data, isLoading, isError } = useGetAvatarQuery(projectId)
+  const { selectedValue, dispatch } = useAvatar()
+
+  const avatarListHandler = () => {
+    data && dispatch(changeTotalAvatarData(data))
+    if (!selectedValue.avatarType) {
+      // 사용자가 지정한 아바타 타입이 없을 경우
+      avatarDetailListHandler('avatar1') // 기본값 지정
+    }
+  }
+  const avatarDetailListHandler = (selectedAvatar: string) => {
+    dispatch(changeAvatarDetailList(selectedAvatar))
+  }
+
+  useEffect(() => {
+    data && avatarListHandler()
+  }, [data])
+
+  useEffect(() => {
+    // console.log('생성')
+  }, [])
+
+  if (isLoading || !data) {
+    return <>{/* 로딩 관련 추가 예정 */}</>
+  }
+
+  if (isError) {
+    return <></>
+  }
+
   return (
     <>
-      <ProjectAvatarData />
       <TheHeader />
       <S.Container>
         <S.LeftContainer>
