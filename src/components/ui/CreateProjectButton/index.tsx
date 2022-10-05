629d9f33f0849b886d659d4711868640c7782a8a
heeyoung-c <heeyoungchun.dev@gmail.com>
2022-10-05 20:36:06 +0900

    👽️ api: 프로젝트 생성 api 연결/#124

--- src/components/ui/CreateProjectButton/index.tsx
+++ src/components/ui/CreateProjectButton/index.tsx
@@ -1,10 +1,28 @@
+import { useCreateProjectMutation } from 'api/historyApi'
 import { CreateProjectButtonIcon } from 'components/Icons'
 import React from 'react'
 import * as S from './style'
 
 export const CreateProjectButton = () => {
+  const [createProject] = useCreateProjectMutation()
+
+  const onClickHandler = () => {
+    createProject()
+      .unwrap()
+      .then((payload) => {
+        console.log(payload)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
   return (
-    <S.Button>
+    <S.Button
+      onClick={() => {
+        onClickHandler()
+      }}
+    >
       <S.Top>
         <CreateProjectButtonIcon width='1.8rem' height='1.8rem' />
         <S.Content>프로젝트 생성</S.Content>
