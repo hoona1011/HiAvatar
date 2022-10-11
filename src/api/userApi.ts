bd3cfee45b097662f57eff3a41850ae0f1693781
boksooni <8545652@naver.com>
2022-10-11 13:01:31 +0900

    👽️ api: userApi/#168

--- src/api/userApi.ts
+++ src/api/userApi.ts
@@ -4,31 +4,48 @@ import { useCookies } from 'react-cookie'
 import { Cookies } from 'react-cookie'
 
 const cookies = new Cookies()
+// const [cookies,setCookie, removeCookie] = useCookies()
+
 export interface UserInfo {
   id: string
   password: string
 }
-
+//회원가입
 export const postUserInfo = async (info: UserInfo) => {
   const res = await axios.post('https://hiavatar.minoflower.com/sign-up', info)
 }
-
-export const postUserCheck = async (id: string) =>
-  await axios.post(
+//아이디중복체크
+export const idCheck = async (id: string) => {
+  const response = await axios.post(
     'https://hiavatar.minoflower.com/sign-up/check/duplicate-id',
-    id
+    { id: id }
   )
-
-export const login = async (info: UserInfo) => {
-  const res = await axios.post('https://hiavatar.minoflower.com/login', info)
-
-  console.log('asdasdasdsadasdasd: ', res.data.data.accessToken)
-
-  const token = res.data.data.accessToken
-
-  cookies.set('accessToken', token, { path: '/' })
+  console.log(id)
+  console.log(response)
+  return response
 }
-
+//로그인
+export const login = async (info: UserInfo) => {
+  // const res = await axios.post('https://hiavatar.minoflower.com/login', info)
+  // console.log('userApi.ts: ', res.data.data.accessToken)
+  // const token = res.data.data.accessToken
+  // cookies.set('accessToken', token, { path: '/' })
+  const response = await axios.post(
+    'https://hiavatar.minoflower.com/login',
+    info
+  )
+  return response
+}
+//간편로그인
+// export const kakao = async () => {
+//   await axios.post('https://hiavatar.minoflower.com/oauth2/authorization/kakao')
+// }
+export const google = async () => {}
+//비밀번호 변경
+export const edit = async () => {
+  const response = await axios.post('https://hiavatart.minoflower.com/my-page')
+  return response
+}
 // const url = import.meta.env.VITE_SERVICE_URL
 
 // export const signUpApi = createApi({
