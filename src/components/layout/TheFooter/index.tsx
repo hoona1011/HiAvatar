5ee6e25fff31a7dc4a3a08e8fe2e66dcda585ce0
boksooni <8545652@naver.com>
2022-10-11 13:02:13 +0900

    💄 design: 푸터 디자인 수정/#168

--- src/components/layout/TheFooter/index.tsx
+++ src/components/layout/TheFooter/index.tsx
@@ -1,18 +1,20 @@
+import { LogoIcon } from 'components/Icons'
 import React from 'react'
 import * as S from './style'
 
 export const TheFooter = () => {
-  return <S.FooterContainer>
-    <S.FooterTextWrapper>
-      <S.FooterTextTop>
-        대표 박철민 | 서울특별시 마포구 월드컵북로 396 <br/>
-        admin@aipark.ai | 전화번호 02-6925-6028 
-      </S.FooterTextTop>
-      <S.FooterTextBot>
-        ⓒ AIPARK CO.,LTD All rights reserved
-      </S.FooterTextBot>
-    </S.FooterTextWrapper>
-    <S.FooterLogo>
-    </S.FooterLogo>
-  </S.FooterContainer>
+  return (
+    <S.FooterContainer>
+      <S.FooterTextWrapper>
+        <S.FooterTextTop>
+          대표 박철민 | 서울특별시 마포구 월드컵북로 396 <br />
+          admin@aipark.ai | 전화번호 02-6925-6028
+        </S.FooterTextTop>
+        <S.FooterTextBot>ⓒ AIPARK CO.,LTD All rights reserved</S.FooterTextBot>
+      </S.FooterTextWrapper>
+      <S.FooterLogo>
+        <LogoIcon width='100' height='40' />
+      </S.FooterLogo>
+    </S.FooterContainer>
+  )
 }
